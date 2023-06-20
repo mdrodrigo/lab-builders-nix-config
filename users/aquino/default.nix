380071ac7beda6d4faba90be38201c25e9e0710c
Otavio Salvador <otavio@ossystems.com.br>
2023-06-20 17:11:30 -0300

    users: aquino: add SSH key
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- users/aquino/default.nix
+++ users/aquino/default.nix
@@ -7,6 +7,10 @@
     isNormalUser = true;
     extraGroups = [ "docker" "wheel" ];
 
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQChHMlcI8Z4YeHuvjgyCquuYHDUFjACcg+FLLJlPOEw9eigH0c770ReVzusGIICFfHv7uuGtqybJlpY3PmIKR0ne+kwcr/tye4Atbnf9ZqjzkvYkt/YN7l7pyGEGgS9U6uCE3+8uS4wfwcIsIh+bfscRrHtybOPDu18ItbcbLWokdHEOhii31Wy4ylLFQbG/7vpa4WJeg5I+56xECYtXfzzHUM6L9zwsSFHObktJCipqANlBiFXZMhBgWzAbiH+iHBxFH+MqCpayoH8vJ9ami8LO3WybEsGEd0wzZqbeQiIzpaIKhNLluYNWlRZqxaG75QPlTPQ7FPOJCeWqP5Tq9hsNZNRWgsvh1Xb3WV5nDsqEdDn7ON6KpKfEimLPsD8JxXdGwhCST7LEufhgPuM2MoINeivpSHDfxBeSPL29YG2XxPj5+tJjsOfmJwS3ZEOJak2ekxC3/6vqY9aOtso2xDXdOC/fsOAefy15K3xSrNyFOOyUVj8zb9DElfS8yV1Bzc= aquino@work"
+    ];
+
     # Default - used for bootstrapping.
     password = "pw";
   };
