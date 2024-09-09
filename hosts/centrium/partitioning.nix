3ae0e1c58ba1b49dc88b52407c795b54b2e2d44a
Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>
2024-09-09 09:28:56 -0300

    centrium/partitioning: Change the partitioning file format
    Signed-off-by: Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>

--- hosts/centrium/partitioning.nix
+++ hosts/centrium/partitioning.nix
@@ -1,36 +1,31 @@
 {
-  disko.devices = {
-    disk = {
-      main = {
-        device = "/dev/nvme0n1";
-        type = "disk";
-        content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
-              name = "ESP";
-              start = "1MiB";
-              end = "512MiB";
-              bootable = true;
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            }
-            {
-              name = "root";
-              start = "512MiB";
-              end = "100%";
-              part-type = "primary";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            }
-          ];
+  disko.devices.disk.main = {
+    type = "disk";
+    device = "/dev/nvme0n1";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          label = "ESP";
+          start = "1MiB";
+          end = "512MiB";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+
+          };
+        };
+        root = {
+          label = "root";
+          start = "512MiB";
+          end = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
         };
       };
     };
