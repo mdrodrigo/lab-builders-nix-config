8bdc9d055290fc8ecaf28f9cd36a85f9b3fb7bf4
Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>
2024-11-29 16:18:41 -0300

    hosts/hyper/default: Configure NFS yocto directories
    Signed-off-by: Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>

--- hosts/hyper/default.nix
+++ hosts/hyper/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = with inputs.nixos-hardware.nixosModules; [
@@ -19,5 +19,32 @@
     initrd.kernelModules = [ ];
 
     kernelModules = [ "kvm-intel" ];
+
+    kernelParams = [ "nfs.nfs4_disable_idmapping=0" "nfsd.nfs4_disable_idmapping=0" ];
+  };
+
+  users.groups.builder = { };
+
+  users.users.rodrigo.extraGroups = [ "builder" ];
+
+  networking.firewall.allowedTCPPorts = [ 2049 ];
+
+  system.activationScripts.srv =
+    ''
+      mkdir -p /srv/nfs/yocto/download-cache
+      mkdir -p /srv/nfs/yocto/sstate-cache
+      chmod -R g+s /srv/nfs/yocto/*
+      chown -R root:builder /srv/nfs/yocto/*
+      chmod -R 775 /srv/nfs/yocto/*
+      ${pkgs.acl}/bin/setfacl -m d:u::rwX,d:g::rwX,d:o::rX /srv/nfs/yocto/*
+    '';
+
+  services.nfs.server = {
+    enable = true;
+    createMountPoints = true;
+    exports = ''
+      /srv/nfs/yocto/download-cache 10.5.3.0/24(rw,sync,nohide,insecure,no_subtree_check) 10.5.4.0/24(rw,sync,nohide,insecure,no_subtree_check)
+      /srv/nfs/yocto/sstate-cache   10.5.3.0/24(rw,sync,nohide,insecure,no_subtree_check) 10.5.4.0/24(rw,sync,nohide,insecure,no_subtree_check)
+    '';
   };
 }
