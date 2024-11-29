bd3fd86c913bae1a4f7c0d06424bfd803d548412
Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>
2024-11-29 17:31:36 -0300

    centrium/default: Config the NFS client in Centrium HOST
    Signed-off-by: Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>

--- hosts/centrium/default.nix
+++ hosts/centrium/default.nix
@@ -13,10 +13,39 @@
   boot = {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
+    supportedFilesystems = [ "nfs" ];
 
-    initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" ];
-    initrd.kernelModules = [ ];
+    initrd = {
+      availableKernelModules = [ "nvme" "xhci_pci" "usbhid" ];
+      #supportedFilesystems = [ "nfs" ];
+      #kernelModules = [ "nfs" ];
+    };
 
     kernelModules = [ "kvm-intel" ];
+
+    kernelParams = [ "nfs.nfs4_disable_idmapping=0" "nfsd.nfs4_disable_idmapping=0" ];
+  };
+
+  users.groups.builder = { };
+
+  users.users.rodrigo.extraGroups = [ "builder" ];
+  users.users.otavio.extraGroups = [ "builder" ];
+
+  system.activationScripts.srv =
+    ''
+      mkdir -p /srv/yocto/download-cache
+      mkdir -p /srv/yocto/sstate-cache
+    '';
+
+  fileSystems."/srv/yocto/sstate-cache" = {
+    device = "10.5.4.144:/srv/nfs/yocto/sstate-cache";
+    fsType = "nfs";
+    options = [ "auto" "rw" "defaults" "_netdev" "x-systemd.automount" ];
+  };
+
+  fileSystems."/srv/yocto/download-cache" = {
+    device = "10.5.4.144:/srv/nfs/yocto/download-cache";
+    fsType = "nfs";
+    options = [ "auto" "rw" "defaults" "_netdev" "x-systemd.automount" ];
   };
 }
