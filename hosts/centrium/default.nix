042324e40cc8109e0444c3f3e1683d1d928a9184
Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>
2025-02-26 18:21:33 -0300

    hosts/centrium: Enable bitbake services
    Signed-off-by: Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>

--- hosts/centrium/default.nix
+++ hosts/centrium/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = with inputs.nixos-hardware.nixosModules; [
@@ -18,5 +18,21 @@
     initrd.kernelModules = [ ];
 
     kernelModules = [ "kvm-intel" ];
+  };
+
+  services.bitbake = {
+    enable = true;
+    versions = {
+      "scarthgap" = {
+        package = pkgs.bitbakePackages.bitbake_2_8_7;
+        hashServPort = 8686;
+        prServPort = 8685;
+      };
+      "styhead" = {
+        package = pkgs.bitbakePackages.bitbake_2_10_2;
+        hashServPort = 8786;
+        prServPort = 8785;
+      };
+    };
   };
 }
